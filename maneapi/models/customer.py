2a2eacb47308e3d8d82c28aac68b896af0c51cdf
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-23 15:21:42 -0600

    fixed customer

--- maneapi/models/customer.py
+++ maneapi/models/customer.py
@@ -1,6 +1,7 @@
 """Customer class module"""
 from django.db import models
 from django.contrib.auth.models import User
+from .style import HairStyle
 
 
 class Customer(models.Model):
@@ -8,3 +9,4 @@ class Customer(models.Model):
     stylist = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     date_created = models.DateField(auto_now=True)
+    style = models.ForeignKey(HairStyle, on_delete=models.SET_NULL, null=True)
